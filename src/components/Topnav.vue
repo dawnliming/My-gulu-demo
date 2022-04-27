112094c81261c56e764c2289ba042dbdb7e2ab68
qinyun <759297569@qq.com>
2022-04-27 14:10:17 +0800

    添加 toggleAside 并完善 aside 的切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="topNav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <span class="toggleAside" @click="toggleMenu"></span>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
-      <span class="toggleAside"></span>
     </ul>
   </div>
 </template>
@@ -46,9 +46,21 @@ export default {
       margin: 0 1em;
     }
   }
+
+  > .toggleAside{
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
   @media (max-width: 500px) {
     > .menu{display: none}
     > .logo{margin: 0 auto}
+    > .toggleAside{display: inline-block;}
   }
 }
 </style>
