698000713bb5dc98728463e5eb44d0680d2fbe3d
qinyun <759297569@qq.com>
2022-04-27 10:15:42 +0800

    封装 topnav 组件，删除不用的组件

---
+++ src/components/Topnav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="topNav">
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Topnav'
+}
+</script>
+
+<style lang="scss" scoped >
+.topNav{
+  padding: 16px;
+  display: flex;
+  background: pink;
+  > .logo{
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu{
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li{
+      margin: 0 1em;
+    }
+  }
+}
+</style>
