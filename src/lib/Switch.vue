db7c5cc7b983df20064d3d9b15ae45fc11f82253
qinyun <759297569@qq.com>
2022-04-28 11:26:41 +0800

    优化 Switch 的 CSS 代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -8,10 +8,10 @@
 
 export default {
   name: "Switch",
-  props:{
+  props: {
     value: Boolean
   },
-  setup(props, context){
+  setup(props, context) {
     const toggle = () => {
       context.emit('update:value', !props.value)
     }
@@ -21,17 +21,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  $h: 22px;
-  $h2: $h - 4px;
-  button{
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: #bfbfbf;
-    border-radius: $h/2;
-    position: relative;
-  }
-  span {
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: $h/2;
+  position: relative;
+
+  > span {
     position: absolute;
     top: 2px;
     left: 2px;
@@ -41,19 +41,30 @@ export default {
     border-radius: $h2/2;
     transition: all 250ms;
   }
-  button.checked {
+
+  &.checked {
     background: #1890ff;
   }
-  button.checked > span {
+
+  &.checked > span {
     left: calc(100% - #{$h2} - 2px);
   }
-  button:focus{
+
+  &:focus {
     outline: none;
   }
-  button:active{
-    > span {width: $h2 + 4px}
+
+  &:active {
+    > span {
+      width: $h2 + 4px
+    }
   }
-  button.checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px}
+
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px
+    }
   }
+}
 </style>
