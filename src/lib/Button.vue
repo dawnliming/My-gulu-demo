3c6739317abc43a415c09d0ebee5c9c84a64ac8d
qinyun <759297569@qq.com>
2022-05-10 14:48:08 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,17 +1,33 @@
 <template>
-  <button class="gulu-button" :class="`gulu-theme-${theme}`">
+  <button class="gulu-button" :class="classes">
     <slot />
   </button>
 </template>
 
 <script>
+import {computed} from "vue";
+
 export default {
   name: "Button",
   props:{
     theme: {
       type: String,
       default: 'button'
-    }
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props) {
+    const { theme, size } = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+      };
+    });
+    return { classes };
   }
 }
 </script>
@@ -54,6 +70,35 @@ div{
 
     &::-moz-focus-inner {
       border: 0;
+    }
+    &.gulu-theme-link{
+      border-color: transparent;
+      box-shadow: none;
+      color: $blue;
+      &:hover,&:focus{
+        color: lighten($blue, 10%);
+      }
+    }
+    &.gulu-theme-text{
+      border-color: transparent;
+      box-shadow: none;
+      color: inherit;
+      &:hover,
+      &:focus {
+        background: darken(white, 5%);
+      }
+    }
+    &.gulu-theme-button{
+      &.gulu-size-big{
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px
+      }
+      &.gulu-size-small{
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
+      }
     }
   }
 }
