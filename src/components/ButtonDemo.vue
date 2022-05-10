f1256bec0f5be7bf85808dad36acdf71006b8d94
qinyun <759297569@qq.com>
2022-05-10 11:30:51 +0800

    创建 Button 组件，添加 theme 属性，完成基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,19 @@
 <template>
   <div>Button</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import Button from "../lib/Button.vue";
 export default {
-  name: "ButtonDemo"
+  name: "ButtonDemo",
+  components: {Button}
 }
 </script>
 
