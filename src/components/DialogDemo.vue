b967305943847036f21f6e95486fe237250665b3
qinyun <759297569@qq.com>
2022-05-11 10:51:20 +0800

    创建 Dialog 组件，并完成基本样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,14 @@
 <template>
   <div>Dialog</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
-<script>
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
 export default {
-  name: "DialogDemo"
+  name: "DialogDemo",
+  components: {Dialog},
 }
 </script>
 
