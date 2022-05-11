365b9038b304bda9bbd06e571ab88ad22212f56c
qinyun <759297569@qq.com>
2022-05-11 17:20:58 +0800

    Dialog 实现一句话 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,22 +1,25 @@
 <template>
   <div>Dialog</div>
   <h1>示例1</h1>
-  <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
-    <template v-slot:content>
-      <strong>hi</strong>
-      <div>hi2</div>
-    </template>
-    <template v-slot:title>
-      <strong>加粗的标题</strong>
-    </template>
-  </Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
-import {ref} from "vue";
+import {openDialog} from "../lib/openDialog";
+import {h, ref} from "vue";
 
 export default {
   name: "DialogDemo",
@@ -31,7 +34,19 @@ export default {
     }
     const f2 = () => {
     }
-    return {x, toggle, f1, f2}
+    const showDialog = () => {
+      openDialog({
+        title: h('strong',{},'标题'),
+        content:'你好',
+        ok(){
+          console.log('ok');
+        },
+        cancel(){
+          console.log('cancel');
+        }
+      })
+    }
+    return {x, toggle, f1, f2, showDialog}
   }
 }
 </script>
