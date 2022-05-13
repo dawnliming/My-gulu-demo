9b8ff5f3087d01a35ff02607130810e226ef41a1
qinyun <759297569@qq.com>
2022-05-13 18:08:53 +0800

    DOC 选中高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -81,18 +81,25 @@ export default {
 aside{
   background: lightblue;
   width: 150px;
-  padding: 16px;
   position: fixed;
   top: 0;
   left: 0;
-  padding-top: 70px;
+  padding: 70px 0 16px;
   height: 100%;
   > h2{
     margin-bottom: 4px;
+    padding: 0 16px;
   }
   > ol{
     > li{
-      padding: 4px 0;
+      >a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
     }
   }
 }
